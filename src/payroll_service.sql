796602de77ae9a312fd7776a7ad9f1419def5a23
ujjawalgit <57063174+ujjawalgit@users.noreply.github.com>
2021-07-14 02:04:14 +0530

    Added: Ability to create a payroll service database

--- src/payroll_service.sql
+++ src/payroll_service.sql
@@ -1,1 +1,34 @@
 "Welcome to Employee PayRoll service"
+
+#UC1: create database
+mysql> CREATE DATABASE payroll_service;
+Query OK, 1 row affected (0.27 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| classicmodels      |
+| information_schema |
+| mysql              |
+| newschema          |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+9 rows in set (0.51 sec)
+
+mysql> USE payroll_service;
+Database changed
+mysql> SELECT DATABASE();
+
+mysql> SELECT DATABASE()
+    -> ;
++-----------------+
+| DATABASE()      |
++-----------------+
+| payroll_service |
++-----------------+
+1 row in set (0.00 sec)
